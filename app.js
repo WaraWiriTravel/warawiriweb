a40a2963c09f08616c380b1292cb806d30bc984f
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2023-10-11 12:46:27 +0700

    feat: inisialisasi project ejs

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  //   res.render("home");
+});
+
+app.listen(2023, function () {
+  console.log("Server berjalan di http://localhost:2023");
+});
